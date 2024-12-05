b99ffa7ba14e9875a7bf22acd62b93834f7bfc8b
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-12-05 09:22:21 +0545

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Import BrowserRouter and Routes
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import ImageSlider from './Components/ImageSlider/ImageSlider';
 import Cart from './Components/cart/Cart';
@@ -11,22 +11,42 @@ import ImageThree from './Components/ImageThree/ImageThree';
 import MiddleLineLayout from './Components/MiddleLineLayout/MiddleLineLayout';
 import Footer from './Components/Footer/Footer';
 import Imageabout from './Components/About/ImageAbout/Imageabout';
+import ImagePart from './Components/About/ImagePart/ImagePart';
+
+// Home layout component
+function Home() {
+  return (
+    <>
+      <ImageSlider />
+      <Cart />
+      <TwoColumnLayout />
+      <ImageRow />
+      <TwoRowLayer />
+      <ImageHand />
+      <ImageThree />
+      <MiddleLineLayout />
+      <Footer />
+    </>
+  );
+}
+
+// About layout component
+function About() {
+  return (
+    <>
+      <Imageabout />
+      <ImagePart />
+    </>
+  );
+}
 
 function App() {
   return (
-    <Router>  {/* Wrap app with BrowserRouter */}
+    <Router>
       <Navbar />
-      <Routes>  {/* Define your routes */}
-        <Route path="/" element={<ImageSlider />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/two-column" element={<TwoColumnLayout />} />
-        <Route path="/image-row" element={<ImageRow />} />
-        <Route path="/two-row" element={<TwoRowLayer />} />
-        <Route path="/image-hand" element={<ImageHand />} />
-        <Route path="/image-three" element={<ImageThree />} />
-        <Route path="/middle-line" element={<MiddleLineLayout />} />
-        <Route path="/footer" element={<Footer />} />
-        <Route path="/about" element={<Imageabout />} /> {/* Route for Imageabout */}
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Home route with all components */}
+        <Route path="/about" element={<About />} /> {/* About route */}
       </Routes>
     </Router>
   );
---
+++ src/Components/About/ImagePart/ImagePart.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./ImagePart.css";
+import ImgShowroom from "../../../assets/showroom.jpg";
+
+const ImagePart = () => {
+  return (
+    <div className="container">
+      <img src={ImgShowroom} alt="Top Image" className="top-image" />
+      <div className="text-box">
+        <div className="left-text">
+          <h2>Leading Gold and Diamond Jewellers</h2>
+          <h4>OUR SPECIALITY</h4>
+          <p>
+            Ganapati jewellers deals in the finest diamond, gold and silver
+            jewellery. Our diamond collection can be worn to impress any crowd
+            at a cocktail party or to wow the entire audience at a wedding.
+          </p>{" "}
+          <br />
+          <p>
+            From exquisite daily wear rings to beautiful bridal Necklaces,
+            Ganapati Jewellers has every kind of piece to add to your
+            beauty.Ganapati Jewellers deals in the highest quality of 24 karat
+            jewellery and the most wonderful designs of 22 karat jewellery. We
+            guarantee highest degree of purity and the finest and most detailed
+            craftsmanship in all our products. Our 22 karat jewellery also
+            includes the most beautiful and rare antique collection. Each
+            Antique jewellery is custom made after several rounds of designing
+            and detailing. Our Antique collection adds a lot of depth to any
+            outfit and is perfect for a show stopping statement or marquee
+            bridal wear
+          </p>
+          <br />
+          <p>
+            Ganapati jewellers also guarantees the purest silver jewellery,
+            ornaments and utensils. The purity we state is the purity we
+            guarantee.
+          </p>
+        </div>
+        <div className="right-text">
+            <p>This is the right side text.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImagePart;
